091f14f0f22ec433ac5d196ddabb44a924fa77d6
Hbrown16 <hbrown1094@gmail.com>
2021-07-21 11:10:03 -0400

    testing licensing q

--- index.js
+++ index.js
@@ -27,6 +27,26 @@ const questions = [
         name: "Operate",
         message: "Provide information on how to use",
     },
+    {
+        type: "input",
+        name: "contribute",
+        message: "What did you contribute?",
+    },
+    {
+        type: "prompt",
+        name: "testing",
+        message: "If you wish to run a test, enter this command.",
+        default: "npm run test",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What license is this under?",
+        choices: ["GitLab", "Github", "PyPI"],
+    },
+    {
+        type: "input"
+    }
 ];
 
 // TODO: Create a function to write README file
