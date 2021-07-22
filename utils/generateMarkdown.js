01b2335236c36e659ac2e01fb5d65a4fc7a145b4
Hbrown16 <hbrown1094@gmail.com>
2021-07-22 19:05:20 -0400

    license final fun

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -23,10 +23,11 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if() {
-    return ;
+  if(license !== 'None') {
+    return `##license
+  ${license} license used.`;  
   }
-  return 
+  return ``;
 }
 
 // TODO: Create a function to generate markdown for README
